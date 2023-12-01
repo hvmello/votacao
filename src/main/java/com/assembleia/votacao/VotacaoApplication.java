3af580b9dcfe8ab43af93e59a8dd14b60e6d2963
Heitor Vieira Mello <heitor29_@hotmail.com>
2023-11-30 21:18:37 -0300

    Update VotacaoApplication.java
    Criação de raiz para API

--- src/main/java/com/assembleia/votacao/VotacaoApplication.java
+++ src/main/java/com/assembleia/votacao/VotacaoApplication.java
@@ -3,6 +3,7 @@ package com.assembleia.votacao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -12,9 +13,15 @@ public class VotacaoApplication {
         SpringApplication.run(VotacaoApplication.class, args);
     }
 
+    @RequestMapping("/")
+    public String home(){
+        return "API Votação!";
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
 
 }
